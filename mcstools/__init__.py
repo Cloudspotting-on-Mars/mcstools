206a9858bd185225c6b86dca45877b414ab7c16c
Marek <marek.j.slipski@jpl.nasa.gov>
2022-12-22 15:49:31 -0800

    fix imports for pip install

--- mcstools/__init__.py
+++ mcstools/__init__.py
@@ -1,4 +1,6 @@
-from .data_path_handler import *
-from .reader import *
-from .loader import *
-from .mcsfile import *
+from data_path_handler import *
+from reader import *
+from loader import *
+from mcsfile import *
+from preprocess import data_pipeline, l1b_limb
+from util import geom, log, mars_time, time
--- setup.py
+++ setup.py
@@ -1,29 +1,3 @@
-from setuptools import find_packages, setup
-
-setup(
-    name='mcstools',
-    version='0.1.0',
-    author='Marek Slipski',
-    author_email='marek.slipski@gmail.com',
-    packages=find_packages(),
-    package_dir={'':'mcstools'},
-    #url='http://pypi.python.org/pypi/PackageName/',
-    license='LICENSE',
-    description='Package to read MCS data',
-    long_description=open('README.md').read(),
-    install_requires=[
-        "black==22.10.0",
-        "coloredlogs==15.0.1",
-        "dask==2022.6.1",
-        "distributed==2022.6.1",
-        "flake8==4.0.1",
-        "isort==5.10.1",
-        "marstime==0.5.6",
-        "numpy==1.23.0",
-        "pandas==1.4.3",
-        "pytest==7.1.2",
-        "requests",
-        "scipy==1.8.1",
-        "xarray",
-    ],
-)
+from setuptools import setup
+if __name__ == '__main__':
+    setup()
