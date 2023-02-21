b5e22ea204c9497d01ab65597c3f4ae252cd9824
Marek <marek.j.slipski@jpl.nasa.gov>
2023-02-21 13:32:26 -0800

    add util fnc to convert to sc frame

--- mcstools/util/test_util.py
+++ mcstools/util/test_util.py
@@ -1,6 +1,7 @@
+import numpy as np
 import pytest
 
-from mcstools.util.geom import scattering_angle, spherical_to_cartesian
+from mcstools.util.geom import scattering_angle, spherical_to_cartesian, mcs_view_angle_to_sc_body_frame
 
 
 def test_s2c(sphere_coords, cart_coords):
@@ -10,6 +11,13 @@ def test_s2c(sphere_coords, cart_coords):
     convert = spherical_to_cartesian(sphere_coords)
     assert convert == pytest.approx(cart_coords)
 
+def test_mcs_sc_transform():
+    sc_frame_0 = mcs_view_angle_to_sc_body_frame(60.4, 261.6)
+    sc_frame_1 = mcs_view_angle_to_sc_body_frame(110.1, 270)
+    sc_frame_2 = mcs_view_angle_to_sc_body_frame(111, 180)
+    assert pytest.approx(sc_frame_0, .005) == np.array([0.127, 0.860, -0.494])
+    assert pytest.approx(sc_frame_1, .005) == np.array([0, 0.939, 0.344])
+    assert pytest.approx(sc_frame_2, .005) == np.array([0.933, 0 , 0.360])
 
 def test_scattering_angle(cart_coords):
     """
