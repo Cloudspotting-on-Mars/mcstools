968482ec10a2bf10e357b8827a2162384a6cfcac
Marek <marek.j.slipski@jpl.nasa.gov>
2024-09-16 13:40:42 -0700

    add common click options

--- mcstools/util/io.py
+++ mcstools/util/io.py
@@ -1,4 +1,29 @@
+import os
+
+import click
 import yaml
+
+
+def mcs_data_loader_click_options(f):
+    "Common options for setting up MCS Loader"
+    f = click.option(
+        "--pds",
+        is_flag=True,
+        default=False,
+        help="Load L2 data from PDS [if False will load from MCS_DATA_PATH]",
+    )(f)
+    f = click.option(
+        "--mcs-data-path",
+        type=str,
+        default=None,
+        help="Path to MCS data path "
+        "[if PDS=False and no path provided, will setup via .env]",
+    )(f)
+    return f
+
+
+def makedirs(output_path):
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
 
 def load_yaml(path):
