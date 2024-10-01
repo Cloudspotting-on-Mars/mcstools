0c948422437db523c682c99ac0089d07605412fb
Marek Slipski <marek.j.slipski@jpl.nasa.gov>
2024-09-30 18:21:08 -0700

    add utc to rad profile

--- mcstools/radiance_profile.py
+++ mcstools/radiance_profile.py
@@ -8,14 +8,15 @@ DETECTOR_POSITIONS = DetectorPositions()
 
 class RadianceProfile:
     def __init__(
-        self, channel: str, radiance_detector_profile: pd.Series, altitudes=None
+        self, channel: str, radiance_detector_profile: pd.Series, altitudes=None, utc=None
     ):
         self.channel = channel
         self.profile = radiance_detector_profile
         self.altitudes = altitudes
+        self.utc = utc
 
     @classmethod
-    def from_l1b_row(self, channel, row, include_altitudes=True):
+    def from_l1b_row(self, channel, row, include_altitudes=True, include_utc=True):
         l1b_file = L1BFile()
         extract_columns = l1b_file.make_rad_col_names(channel)
         detectors = [int(x.split("_")[-1]) for x in extract_columns]
@@ -34,7 +35,12 @@ class RadianceProfile:
                 row["Scene_alt"],
                 DETECTOR_POSITIONS.elevation.loc[profile.index, channel],
             )
-        return RadianceProfile(channel, profile, altitudes=altitudes)
+        if include_utc:
+            if "dt" in row.index:
+                utc = row['dt']
+            else:
+                raise ValueError("dt column not found in L1B row")
+        return RadianceProfile(channel, profile, altitudes=altitudes, utc=utc)
 
     def __str__(self):
         return f"Channel {self.channel}. Profile:\n{self.profile}"
